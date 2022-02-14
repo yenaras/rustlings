78d601ab5dbe9c2a1f4c6dcdf12d8f52a45a1c04
brandon82890 <brandon82890@gmail.com>
2022-02-13 23:59:38 -0500

    initial commit

---
+++ exercises/enums/enums2.rs
@@ -0,0 +1,31 @@
+// enums2.rs
+// Make me compile! Execute `rustlings hint enums2` for hints!
+
+
+#[derive(Debug)]
+enum Message {
+    Move{x: u8, y: u8},
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
+    // TODO: define the different variants used below
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", &self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
